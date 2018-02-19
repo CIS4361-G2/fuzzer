4879ee7539628be13273eb86023f861896dac179
richard.z <rlziii@icloud.com>
2018-02-19 13:22:43 -0500

    Changed copyJPG() to take in a file pointer for the source, instead of building one via a string

--- fuzzer.c
+++ fuzzer.c
@@ -2,43 +2,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Copy a JPG file to a new file
-FILE *copyJPG(char *jpgInputFileName, char *jpgCopyFileName)
-{
-    FILE *jpgSource, *jpgCopy;
+// Set to 1 to print debug messages
+// Set to 0 to suppress debug messages
+#define DEBUG 1
 
+// Clone a JPG file into a newly created JPG file
+// jpgInputFile must already be openned elsewhere
+// jpgCopyFileName will be opened in this function
+FILE *copyJPG(FILE *jpgSource, char *jpgCopyFileName)
+{
+    FILE *jpgCopy;
     int i = 0;
     int jpgLength;
 
-    jpgSource = fopen(jpgInputFileName, "rb");
+    // Open the JPG source file in read-binary mode
     if (jpgSource == NULL) {
-        printf("FUZZER: Cannot open sample.jpg");
+        printf("FUZZER: Cannot open source JPG\n");
         return NULL;
     }
 
+    // Read through the end of the JPG file to count the length
     fseek(jpgSource, 0, SEEK_END);
     jpgLength = ftell(jpgSource);
     fseek(jpgSource, 0, SEEK_SET);
 
+    // Attempt to create a new file in write-binary mode (for JPG copy)
     jpgCopy = fopen(jpgCopyFileName, "wb");
-
     if (jpgCopy == NULL) {
-        printf("FUZZER: Cannot create sample_copy.jpg");
+        printf("FUZZER: Cannot create %s\n", (jpgCopyFileName != NULL ? jpgCopyFileName : "JPG copy"));
         fclose(jpgSource);
         return NULL;
     }
 
+    // Copy the JPG file byte by byte
     for (i = 0; i < jpgLength; i++) {
         fputc(fgetc(jpgSource), jpgCopy);
     }
 
     printf("FUZZER: Image copied successfully.\n");
 
+    // Ensure that the source file is closed
     fclose(jpgSource);
 
     return jpgCopy;
 }
 
+// Generate the string used for the system call to execute the jpg2pdf program
 char *createSystemString(char *jpgFileName)
 {
     char *systemString;
@@ -48,6 +57,7 @@ char *createSystemString(char *jpgFileName)
 
     systemString = malloc(sizeof(char) * 72);
 
+    // Example: ./jpg2pdf example.jpg > /dev/null
     strcpy(systemString, "");
     strcpy(systemStringA, "./jpg2pdf ");
     strcpy(systemStringB, jpgFileName);
@@ -62,22 +72,34 @@ char *createSystemString(char *jpgFileName)
 
 int main(int argc, char *argv[])
 {
-    // Copy the source JPG to ensure that we don't modify the source directly
-    FILE *jpgCopy = copyJPG(argv[1], argv[2]);
+    // Open the source JPG file
+    FILE *jpgSource = fopen(argv[1], "rb");
+    if (jpgSource == NULL) {
+        printf("FUZZER: Cannot open source JPG\n");
+        return -1;
+    }
+
+    // Create a copy of the source JPG file
+    FILE *jpgCopy = copyJPG(jpgSource, argv[2]);
+    if (jpgCopy == NULL) {
+        printf("FUZZER: Cannot create %s\n", (argv[2] != NULL ? argv[2] : "JPG copy"));
+        fclose(jpgSource);
+        return -1;
+    }
 
     // Create the command to have the system execute
     char *systemString = createSystemString(argv[2]);
 
-    // Can be used to ensure that the systemString is correctly built
-    // printf("%s\n", systemString);
-
     int i = 0;
+
+    // Can be used to ensure that the systemString is correctly built
+    if (DEBUG) printf("%s\n", systemString);
 
     // This is only set to 50 for testing purposes
     while (i <= 50) {
+        // Example: system("./jpg2pdf example.jpg > /dev/null");
         // "> /dev/null" will redirect the output of jpg2pdf to nowhere
         // This prevents the output of jpg2pdf from printing to the terminal
-        // system("./jpg2pdf sample.jpg > /dev/null");
         if (system(systemString) == 11) {
             printf("CRASH\n");
             return 0;
@@ -89,17 +111,18 @@ int main(int argc, char *argv[])
         // This gives the effect that the number is updating in place
         printf("\r%d iterations...", i++);
 
-        // This ensures that C prints every line, not just sometimes
+        // This ensures that every line is printed (not just some of them)
         fflush(stdout);
     }
 
     printf("\n");
 
+    // Clean up
     free(systemString);
     fclose(jpgCopy);
 
     // Signal that the program reached the end of execution...
-    printf("The end of the road...\n");
+    if (DEBUG) printf("The end of the road...\n");
 
     return 0;
 }
