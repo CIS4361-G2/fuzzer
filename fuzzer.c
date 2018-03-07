67125acdf8955e1e4ff4a1449b4c67a8dcb0d330
richard.z <rlziii@icloud.com>
2018-03-07 13:21:58 -0500

    Simplified the JPG copy's file name

--- fuzzer.c
+++ fuzzer.c
@@ -4,12 +4,17 @@
 
 // Set to 1 to print debug messages
 // Set to 0 to suppress debug messages
-#define DEBUG 0
+#define DEBUG 1
 
-typedef struct Jpgfileinfo {
-	FILE *jpgFile;
-	char *fileName;
-	unsigned long fileSize;
+// System exit codes
+#define SEGMENTATION_FAULT 35584
+
+// The name for the generated JPG file(s)
+#define JPG_FILE "fuzzer_input_file.jpg"
+
+typedef struct JPGFile {
+    FILE *jpgFile;
+    unsigned long fileSize;
 } JPGFile;
 
 // Modifies the given number of bits using
@@ -17,88 +22,38 @@ typedef struct Jpgfileinfo {
 // JPGFile itself is modified, so nothing
 // is returned.
 void modifyBits(JPGFile *file, int startBit, int endBit, int bitsToChange, int excludeFirst, int excludeLast) {
-	int numBits = file->fileSize * 8;
-	int *bits = (int*)calloc(numBits, sizeof(int));
-	int i = 0;
-	int k = 0;
-	char curChar;
+    int numBits = file->fileSize * 8;
+    int *bits = (int*)calloc(numBits, sizeof(int));
+    int i = 0;
+    int k = 0;
+    char curChar;
 
-	fseek(file->jpgFile, 0, SEEK_SET);
+    fseek(file->jpgFile, 0, SEEK_SET);
 
-	for (i = 0; i < file -> fileSize; i++) {
-		curChar = fgetc(file->jpgFile);
+    for (i = 0; i < file -> fileSize; i++) {
+        curChar = fgetc(file->jpgFile);
 
-		for (k = 0; k < 8; k++) {
-			// Not 100% sure that this is correct.
-			bits[i + k] = (curChar & 128);
-			curChar <<= 1;
-		}
-	}
-}
-
-// Build a file name string with "_copy" appended
-// Example: example.jpg -> example_copy.jpg
-char *createJPGCopyFileName(char *jpgSourceFileName)
-{
-    char *appendedString;
-    char appendage[6];
-
-    int i;
-
-    appendedString = malloc(sizeof(char) * (strlen(jpgSourceFileName) + 5));
-
-    // Ensure that the string is null-terminated
-    // This prevents garbage from being present at the beginning of the string
-    appendedString[0] = '\0';
-
-    // This is what will be appended to the string
-    strcpy(appendage, "_copy");
-
-    // Find where the '.' character in the filename is located
-    for (i = 0; i < strlen(jpgSourceFileName); i++) {
-        if (jpgSourceFileName[i] == '.') {
-            break;
+        for (k = 0; k < 8; k++) {
+            // Not 100% sure that this is correct.
+            bits[i + k] = (curChar & 128);
+            curChar <<= 1;
         }
     }
-
-    // Copy the filename up to (not including) the '.' character
-    // Example: "example" <- example.jpg
-    strncat(appendedString, jpgSourceFileName, i);
-
-    // Add "_copy" to the filename
-    // Example: "example_copy" <- example.jpg
-    strcat(appendedString, appendage);
-
-    // Add the rest of the filename
-    // From (and including) '.' to the end of the filename
-    // Example: "example_copy.jpg" <- example.jpg
-    strcat(appendedString, &jpgSourceFileName[i]);
-
-    // Print the resulting string
-    if (DEBUG) printf("TEST: %s\n", appendedString);
-
-    return appendedString;
 }
 
 // Clone a JPG file into a newly created JPG file
 // jpgInputFile must already be openned elsewhere
 // jpgCopyFileName will be opened in this function
-JPGFile *copyJPG(FILE *jpgSource, char *jpgCopyFileName)
+JPGFile *copyJPG(FILE *jpgSource)
 {
     FILE *jpgCopy;
     int i = 0;
     int jpgLength;
-	JPGFile *file = (JPGFile*)malloc(sizeof(JPGFile));
+    JPGFile *file = malloc(sizeof(JPGFile));
 
-	// Prepare our struct to store file information
-	if (file == NULL) {
-		printf("FUZZER: Unable to allocate memory for an object.\n");
-		return NULL;
-	}
-
-    // Open the JPG source file in read-binary mode
-    if (jpgSource == NULL) {
-        printf("FUZZER: Cannot open source JPG\n");
+    // Prepare our struct to store file information
+    if (file == NULL) {
+        printf("FUZZER: Unable to allocate memory for copied JPG file.\n");
         return NULL;
     }
 
@@ -107,13 +62,11 @@ JPGFile *copyJPG(FILE *jpgSource, char *jpgCopyFileName)
     jpgLength = ftell(jpgSource);
     fseek(jpgSource, 0, SEEK_SET);
 
-    // Append "_copy" to the source file
-
-
     // Attempt to create a new file in write-binary mode (for JPG copy)
-    jpgCopy = fopen(jpgCopyFileName, "wb");
+    jpgCopy = fopen(JPG_FILE, "wb");
+
     if (jpgCopy == NULL) {
-        printf("FUZZER: Cannot create %s\n", (jpgCopyFileName != NULL ? jpgCopyFileName : "JPG copy"));
+        printf("FUZZER: Cannot create copied JPG file.\n");
         fclose(jpgSource);
         return NULL;
     }
@@ -123,57 +76,41 @@ JPGFile *copyJPG(FILE *jpgSource, char *jpgCopyFileName)
         fputc(fgetc(jpgSource), jpgCopy);
     }
 
-    printf("FUZZER: Image copied successfully.\n");
     file->jpgFile = jpgCopy;
-	if (DEBUG) printf("Stored jpgCopy successfully.\n");
-	file->fileName = jpgCopyFileName;
-	if (DEBUG) printf("Stored jpgCopyFileName successfully.\n");
-	file->fileSize = jpgLength;
-	if (DEBUG) printf("Stored jpgLength successfully.\n");
+    file->fileSize = jpgLength;
 
-	if (DEBUG) printf("Created JPGFile successfully.\n");
-	return file;
-}
-
-// Generate the string used for the system call to execute the jpg2pdf program
-char *createSystemString(char *jpgFileName, int fileNumber)
-{
-    char *systemString = malloc(sizeof(char) * 100);
-
-    sprintf(systemString, "./jpg2pdf-%d %s > /dev/null", fileNumber, jpgFileName);
-
-    if (DEBUG) printf("System String: %s\n", systemString);
-    return systemString;
+    return file;
 }
 
 int main(int argc, char *argv[])
 {
-    // A set of strings that represent the jpg2pdf versions
-    char version[10][10] = {
-        "jpg2pdf-0", "jpg2pdf-1", "jpg2pdf-2", "jpg2pdf-3", "jpg2pdf-4",
-        "jpg2pdf-5", "jpg2pdf-6", "jpg2pdf-7", "jpg2pdf-8", "jpg2pdf-9",
-    };
-
     char *jpgSourceName = argv[1];
-    int start = atoi(argv[2]);
-    int end = atoi(argv[3]);
+    int start;
+    int end;
     FILE *jpgSource = NULL;
-    char *systemString = NULL;
+    char systemString[50];
 
-    if (DEBUG) printf("1: %s\n2:%s\n3:%s\n", argv[1], argv[2], argv[3]);
+    if (DEBUG) printf("\n");
+    if (DEBUG) printf("COMMAND-LINE ARGUMENTS\n");
+    if (DEBUG) printf("argv[0]: %s\n", argv[0]);
+    if (DEBUG) printf("argv[1]: %s\n", argv[1]);
+    if (DEBUG) printf("argv[2]: %s\n", argv[2]);
+    if (DEBUG) printf("argv[3]: %s\n\n", argv[3]);
 
-	if (argc != 4) {
-		printf("Error: Incorrect execution format.\n");
-		printf("Execution format: fuzzer [JPG] [start] [end]\n");
+    // Ensure that the command-line arguments are all passed correctly
+    if (argc != 4) {
+        printf("Error: Incorrect execution format.\n");
+        printf("Execution format: fuzzer [JPG] [start] [end]\n");
         printf("Example: fuzzer example.jpg 0 9\n");
         printf("[JPG] is a JPG file that needs to include .jpg\n");
         printf("[start] and [end] represent the range of bugs being tested\n");
         printf("[start] and [end] should be integers between 0 and 9\n");
-		return 0;
-	}
 
-    // TEST
-    char *jpgCopyFileName = createJPGCopyFileName(jpgSourceName);
+        return 0;
+    }
+
+    start = atoi(argv[2]);
+    end = atoi(argv[3]);
 
     // Open the source JPG file
     jpgSource = fopen(jpgSourceName, "rb");
@@ -183,9 +120,8 @@ int main(int argc, char *argv[])
     }
 
     // Create a copy of the source JPG file
-	JPGFile *jpgCopy = copyJPG(jpgSource, jpgCopyFileName);
+    JPGFile *jpgCopy = copyJPG(jpgSource);
     if (jpgCopy == NULL) {
-        // This line needs to be fixed (argv[2] is no longer the copy string)
         printf("FUZZER: Cannot create JPG copy");
         fclose(jpgSource);
         return -1;
@@ -200,19 +136,19 @@ int main(int argc, char *argv[])
 
     for (i = start; i <= end; i++)
     {
-        // Create the command to have the system execute
-        char *systemString = createSystemString(jpgCopyFileName, i);
-
         crashDetected = 0;
 
-        // This currently isn't working; the return value isn't correct
         while (!crashDetected) {
             /* generate JPG */
 
-            systemReturnValue = system(systemString);
-            if (DEBUG) printf(" *** %d ***", systemReturnValue);
+            sprintf(systemString, "./jpg2pdf-%d fuzzer_input_file.jpg > /dev/null", i);
+            puts(systemString);
 
-            if (systemReturnValue == 11) {
+            systemReturnValue = system(systemString);
+
+            if (DEBUG) printf("systemReturnValue: %d\n", systemReturnValue);
+
+            if (systemReturnValue == SEGMENTATION_FAULT) {
                 printf("\nProgram crash in jpg2pdf-%d!\n", i);
                 // Save the JPG file
                 crashDetected = 1;
@@ -223,11 +159,12 @@ int main(int argc, char *argv[])
             }
 
             // This is here just for testing
-            if (iterationCount > 100) {
+            if (iterationCount > 10) {
                 break;
             }
 
-            printf("\r%d iterations (%d successes, %d failures)", iterationCount++, successCount, failureCount);
+            iterationCount++;
+            printf("\r%d iterations (%d successes, %d failures)\n\n", iterationCount, successCount, failureCount);
             fflush(stdout);
         }
     }
@@ -235,9 +172,8 @@ int main(int argc, char *argv[])
     printf("\n");
 
     // Clean up
-    free(systemString);
-    fclose(jpgSource);
     free(jpgCopy);
+    fclose(jpgSource);
 
 
     // Signal that the program reached the end of execution...
