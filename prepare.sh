64d2c42b0acebb439fe61a618f7a58172e9f0019
Sydney <shroomdog27@yahoo.com>
2018-04-09 22:46:51 -0400

    Edit prepare.sh to prepare more things.

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
+mkdir exes;
+mkdir imgs;
+gcc -Wall -o fuzzer fuzzer.c;
+
 for i in {0..9};
 do
-    g++ -o exes/jpg2pdf-$i jpg2pdfs/jpg2pdf-$i.cpp;
+    g++ -Wall -o exes/jpg2pdf-$i jpg2pdfs/jpg2pdf-$i.cpp;
 done;
